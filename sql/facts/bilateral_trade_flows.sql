45545f9e004d34759044b3b05ce841f484508670
Franco Piccolo <francopiccolo@gmail.com>
2023-09-26 16:07:58 -0300

    added product categories

--- sql/facts/bilateral_trade_flows.sql
+++ sql/facts/bilateral_trade_flows.sql
@@ -6,11 +6,11 @@ SELECT
         CAST(k AS INTEGER)	AS product_category,
         CAST(i AS INTEGER)      AS exporter_country_code,
         CAST(j AS INTEGER)	AS importer_country_code,
-        CAST(v*1000 AS FLOAT)	AS trade_value_current_usd,
+        CAST(v*1000 AS FLOAT64)	AS trade_value_current_usd,
         SAFE_CAST(q AS FLOAT64) AS qty_tons --some values were NA
 FROM    cepii.bilateral_trade_flows
 JOIN    dim.country ce ON ce.country_code = i 
 JOIN    dim.country ci ON ci.country_code = j
-WHERE   ce.country IN ('Argentina', 'Brazil', 'Bolivia', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Peru', 'Uruguay', 'Venezuela')
-        OR ci.country IN ('Argentina', 'Brazil', 'Bolivia', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Peru', 'Uruguay', 'Venezuela')
+WHERE   ce.country IN ('Argentina', 'Brazil', 'Plurinational State of Bolivia', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Peru', 'Uruguay', 'Venezuela')
+        OR ci.country IN ('Argentina', 'Brazil', 'Plurinational State of Bolivia', 'Chile', 'Colombia', 'Ecuador', 'Paraguay', 'Peru', 'Uruguay', 'Venezuela')
 );
--- sql/views/bilateral_trade_flows.sql
+++ sql/views/bilateral_trade_flows.sql
@@ -5,11 +5,11 @@ CREATE VIEW views.bilateral_trade_flows AS (
 SELECT  tf.year, 
         ce.country          AS exporter_country,
         ci.country          AS importer_country,
-        pc.product_category AS product_category,
+        pc.*,
         tf.trade_value_current_usd,
         tf.qty_tons
 FROM    fact.bilateral_trade_flows tf
 JOIN    dim.country ce ON ce.country_code = tf.exporter_country_code
 JOIN    dim.country ci ON ci.country_code = tf.importer_country_code
-JOIN    dim.product_category pc ON pc.product_category_code = tf.product_category
+JOIN    dim.product_category pc ON pc.product_category_l4_code = tf.product_category
 );
